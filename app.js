4da5d42dab9883791a7c0c0672a0bd6c7d25a5a7
l4ndyn <l4ndyn@gmail.com>
2021-09-12 15:21:03 +0300

    add index

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -17,15 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // error handler
 app.use(function(err, req, res, next) {
